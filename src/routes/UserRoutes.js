72e8fe3a3d3b8cc789a674413ec2904d1310ea48
Identitynull <identitynulll@gmail.com>
2025-11-06 10:20:58 +0500

    a

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -19,7 +19,7 @@ router.post("/login", async (req, res) => {
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) return res.status(400).json({ msg: "Wrong password" });
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET,);
   res.json({ token });
 });
 
